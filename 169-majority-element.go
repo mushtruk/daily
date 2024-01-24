1ec12bb1666091e7a2b4898a66313c4c99a45b8b
Ihor Mushtruk <kurthsum@gmail.com>
2024-01-24 14:40:51 +0200

    day5 - majority element (boyer-moore);

---
+++ 169-majority-element.go
@@ -0,0 +1,36 @@
+package main
+
+// boyer-moore voting algorithm
+// it works unless there are majority element appears at least len/2
+// otherwise second
+
+func majorityElement(nums []int) int {
+	var candidate, count int
+
+	for _, num := range nums {
+		if count == 0 {
+			candidate = num
+		}
+
+		if candidate == num {
+			count++
+		} else {
+			count--
+		}
+	}
+
+	// optional unles count < len/2
+	// count = 0
+	// for _, num := range nums {
+	// 	if candidate == num {
+	// 		count++
+	// 	}
+	// }
+
+	// if count > len(nums)/2 {
+	// 	return candidate
+	// }
+	// return -1
+
+	return candidate
+}
--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-var _nums = []int{1, 1, 1, 2, 2, 3}
+var _nums = []int{3, 2, 3}
 
 func main() {
-	fmt.Print(removeDuplicates2(_nums))
+	fmt.Print(majorityElement(_nums))
 }
