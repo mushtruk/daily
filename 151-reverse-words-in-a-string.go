9594aba70c9f67d29f7bfde6908767940a077ab6
Ihor Mushtruk <kurthsum@gmail.com>
2024-02-09 20:14:19 +0200

    day 21 - reverse words;

---
+++ 151-reverse-words-in-a-string.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"strings"
+)
+
+var regularString = "the sky is blue"
+
+func reverseWords(s string) string {
+	words := strings.Fields(s)
+	reversed := make([]string, 0, len(words))
+
+	for i := len(words) - 1; i >= 0; i-- {
+		reversed = append(reversed, words[i])
+	}
+
+	return strings.Join(reversed, " ")
+}
