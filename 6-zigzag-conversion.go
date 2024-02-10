73fac9237be15d832b1b479b7331659cd32e8bf6
Ihor Mushtruk <kurthsum@gmail.com>
2024-02-10 20:03:16 +0200

    day 22 - zigzag conversion;

---
+++ 6-zigzag-conversion.go
@@ -0,0 +1,39 @@
+package main
+
+var (
+	toZigZag = "PAYPALISHIRING"
+	numRows  = 3
+)
+
+func convert(s string, numRows int) string {
+	if numRows == 1 || len(s) <= numRows {
+		return s
+	}
+
+	var (
+		rows       = make([]string, numRows)
+		currentRow = 0
+		goingDown  = false
+		result     = ""
+	)
+
+	for _, char := range s {
+		rows[currentRow] += string(char)
+
+		if currentRow == 0 || currentRow == numRows-1 {
+			goingDown = !goingDown
+		}
+
+		if goingDown {
+			currentRow += 1
+		} else {
+			currentRow -= 1
+		}
+	}
+
+	for _, row := range rows {
+		result += row
+	}
+
+	return result
+}
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print(reverseWords(regularString))
+	fmt.Print(convert(toZigZag, numRows))
 }
