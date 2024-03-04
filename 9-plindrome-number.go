5b32b0454ffa1949f96f563a480bafd4427967ad
Ihor Mushtruk <kurthsum@gmail.com>
2024-03-05 00:22:25 +0200

    day 45 - is number palindrome (alternative);

--- 9-plindrome-number.go
+++ 9-plindrome-number.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func IsNumberPalindrome(x int) bool {
 	digits := fmt.Sprint(x)
+	// or
+	// digits := splitDigits(x)
 	for i, j := 0, len(digits)-1; i < j; i, j = i+1, j-1 {
 		if digits[i] != digits[j] {
 			return false
