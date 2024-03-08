3e0cf254f3f63f2a77959052eb1d1699f4a8cf61
Ihor Mushtruk <kurthsum@gmail.com>
2024-03-08 22:45:36 +0200

    day 48 - Maximum Depth of Binary Tree (description added);

--- 104-maximum-depth-of-binary-tree.go
+++ 104-maximum-depth-of-binary-tree.go
@@ -1,5 +1,9 @@
 package main
 
+// Given the root of a binary tree, return its maximum depth.
+
+// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
 // * Definition for a binary tree node.
 type TreeNode struct {
 	Val   int
