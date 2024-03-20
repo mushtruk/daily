c0a79289f5428289f7e49009c584d3fc60537d79
Ihor Mushtruk <kurthsum@gmail.com>
2024-03-20 23:30:04 +0200

    day 60 - bs peak element (fix);

--- 162-find-peak-element.go
+++ 162-find-peak-element.go
@@ -10,14 +10,13 @@ package main
 
 func FindPeakElement(nums []int) int {
 	left, right := 0, len(nums)-1
-
-	for left <= right {
+	for left < right {
 		mid := left + (right-left)/2
 
 		if nums[mid] < nums[mid+1] {
 			left = mid + 1
 		} else {
-			right = mid - 1
+			right = mid
 		}
 	}
 
